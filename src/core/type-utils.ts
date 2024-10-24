a2f29a6b3c0316a92ce29c25bf69b08402559166
msyafiqq <muhammad.syafiq@geniebook.com>
2024-10-24 21:07:54 +0700

    feat(helper): add nullable to undefined

--- src/core/type-utils.ts
+++ src/core/type-utils.ts
@@ -1,3 +1,7 @@
 export function nullableToNull<T>(value: T | undefined | null): T | null {
   return value === undefined ? null : value;
 }
+
+export function nullableToUndefined<T>(value: T | null): T | undefined {
+  return value === null ? undefined : value;
+}
