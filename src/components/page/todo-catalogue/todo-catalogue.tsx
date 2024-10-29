34e84af463d7c487073b852a4baef6d082d64f28
msyafiqq <muhammad.syafiq@geniebook.com>
2024-10-29 16:47:43 +0700

    feat(todo-catalogue): 007, add-right button nav

--- src/components/page/todo-catalogue/todo-catalogue.tsx
+++ src/components/page/todo-catalogue/todo-catalogue.tsx
@@ -1,17 +1,37 @@
 import { useQuery } from '@tanstack/react-query';
+import { Stack } from 'expo-router';
 import React, { useEffect } from 'react';
+import { Pressable, View } from 'react-native';
 
 import { useTodoRepository } from '@/api/repositoiry/todo';
 import { ListPage } from '@/components/page/todo-catalogue/components/todo-list-layout';
 import { TodoPageLoading } from '@/components/page/todo-catalogue/components/todo-page-loading';
 import { toPresenter } from '@/components/page/todo-catalogue/todo-presenter+entity';
-import { FocusAwareStatusBar, showErrorMessage } from '@/ui';
+import {
+  hideFilterAndOrder,
+  showFilterAndOrder,
+} from '@/core/state/todo-catalogue-navigation-slice';
+import { useAppDispatch, useAppSelector } from '@/core/state/use-redux';
+import {
+  colors,
+  FocusAwareStatusBar,
+  Modal,
+  showErrorMessage,
+  useModal,
+} from '@/ui';
+import { Sliders } from '@/ui/icons/sliders';
 
 export function TodoCatalogue() {
   const repository = useTodoRepository();
+  const isFilterActive = useAppSelector(
+    (state) => state.todoCatalogueNavigation.isFilterAndOrderShown,
+  );
+  const dispatch = useAppDispatch();
+  const modal = useModal();
 
   const {
     isPending,
+    isFetching,
     error,
     data: todos,
   } = useQuery({
@@ -27,6 +47,14 @@ export function TodoCatalogue() {
     }
   }, [error]);
 
+  useEffect(() => {
+    if (isPending || isFetching) {
+      dispatch(hideFilterAndOrder());
+    } else {
+      dispatch(showFilterAndOrder());
+    }
+  }, [isPending, isFetching]);
+
   if (isPending) {
     return (
       <>
@@ -39,6 +67,25 @@ export function TodoCatalogue() {
   return (
     <>
       <FocusAwareStatusBar />
+      <Modal ref={modal.ref} snapPoints={['95%']}>
+        <></>
+      </Modal>
+      <Stack.Screen
+        options={{
+          headerRight: () =>
+            isFilterActive ? (
+              <Pressable onPress={() => modal.present()}>
+                <View className="items-center p-4">
+                  <Sliders
+                    color={colors.charcoal[700]}
+                    width={20}
+                    height={20}
+                  />
+                </View>
+              </Pressable>
+            ) : null,
+        }}
+      />
       <ListPage data={todos ?? []}></ListPage>
     </>
   );
