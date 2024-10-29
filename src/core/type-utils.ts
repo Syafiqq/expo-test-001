1060f7462b203dc0f2cb94c0e699c751f8540102
msyafiqq <muhammad.syafiq@geniebook.com>
2024-10-29 16:27:06 +0700

    feat(helper): add compactMap

--- src/core/type-utils.ts
+++ src/core/type-utils.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export function nullableToNull<T>(value: T | undefined | null): T | null {
   return value === undefined ? null : value;
 }
@@ -5,3 +7,10 @@ export function nullableToNull<T>(value: T | undefined | null): T | null {
 export function nullableToUndefined<T>(value: T | null): T | undefined {
   return value === null ? undefined : value;
 }
+
+export const compactMap = <T, U>(
+  array: T[],
+  callback: (item: T, index: number, array: T[]) => U | undefined,
+): U[] => {
+  return _.compact(array.map(callback));
+};
