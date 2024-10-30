0a33803d80081e7bc81e648b5b5385ee6478f928
msyafiqq <muhammad.syafiq@geniebook.com>
2024-10-30 23:00:20 +0700

    feat(slice): 008, 01, add custom header

--- src/components/page/todo-catalogue/todo-catalogue.tsx
+++ src/components/page/todo-catalogue/todo-catalogue.tsx
@@ -1,7 +1,11 @@
+import { Header, HeaderBackButton } from '@react-navigation/elements';
+import { type NativeStackHeaderProps } from '@react-navigation/native-stack';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
-import React, { memo, useEffect } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { Pressable, View } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import { debounceTime, Subject } from 'rxjs';
 
 import { useTodoRepository } from '@/api/repositoiry/todo';
 import { ListPage } from '@/components/page/todo-catalogue/components/todo-list-layout';
@@ -11,8 +15,11 @@ import { type TodoPresenter } from '@/components/page/todo-catalogue/todo-presen
 import { toPresenter } from '@/components/page/todo-catalogue/todo-presenter+entity';
 import {
   hideFilterAndOrder,
+  hideSearch,
   showFilterAndOrder,
+  showSearch,
 } from '@/core/state/todo-catalogue-navigation-slice';
+import { updateSearch } from '@/core/state/todo-catalogue-search-slice';
 import { useAppDispatch, useAppSelector } from '@/core/state/use-redux';
 import {
   colors,
@@ -21,18 +28,114 @@ import {
   showErrorMessage,
   useModal,
 } from '@/ui';
+import { FaMagnifyingGlassSolid } from '@/ui/icons/fa-magnifying-glass-solid';
 import { Sliders } from '@/ui/icons/sliders';
 
 const ListPageMemo = memo(({ todos }: { todos: TodoPresenter[] }) => (
   <ListPage data={todos ?? []} />
 ));
 
-// eslint-disable-next-line max-lines-per-function
-export function TodoCatalogue() {
-  const repository = useTodoRepository();
+// MARK: - Search Bar
+
+const debouncedSearchBarSubject = new Subject<string>();
+type DebouncedSearchBarProps = {
+  onChangeText?: ((text: string) => void) | undefined;
+};
+const DebouncedSearchBar = ({ onChangeText }: DebouncedSearchBarProps) => {
+  const [textSearch, setTextSearch] = useState('');
+
+  useEffect(() => {
+    const subscription = debouncedSearchBarSubject
+      .pipe(debounceTime(1000))
+      .subscribe((text) => {
+        onChangeText?.(text);
+      });
+    return () => subscription.unsubscribe();
+  }, [onChangeText]);
+
+  useEffect(() => {
+    debouncedSearchBarSubject.next(textSearch);
+  }, [textSearch]);
+
+  return (
+    <Searchbar
+      placeholder="Search"
+      onChangeText={setTextSearch}
+      value={textSearch}
+    />
+  );
+};
+
+// MARK: - Header
+
+type TodoHeaderProps = NativeStackHeaderProps & {
+  onFilterPress?: (() => void) | undefined;
+};
+const TodoHeader = (props: TodoHeaderProps) => {
+  const isSearchShown = useAppSelector(
+    (state) => state.todoCatalogueNavigation.isSearchShown,
+  );
   const isFilterActive = useAppSelector(
     (state) => state.todoCatalogueNavigation.isFilterAndOrderShown,
   );
+
+  const dispatch = useAppDispatch();
+
+  const onChangeText = (text: string) => {
+    dispatch(updateSearch(text));
+  };
+
+  return isSearchShown ? (
+    <Header
+      title="ToDo"
+      {...props}
+      headerLeft={() => (
+        <HeaderBackButton onPress={() => dispatch(hideSearch())} />
+      )}
+      headerTitle={() => <DebouncedSearchBar onChangeText={onChangeText} />}
+      headerTitleContainerStyle={{
+        flex: 1,
+        marginHorizontal: 0,
+        maxWidth: '100%',
+        marginEnd: 16,
+      }}
+      headerRightContainerStyle={{ flexGrow: 0 }}
+    />
+  ) : (
+    <Header
+      title="ToDo"
+      {...props}
+      headerRight={() => (
+        <View className="mr-4 flex-row">
+          <Pressable
+            onPress={() => {
+              dispatch(showSearch());
+            }}
+          >
+            <View className="items-center p-2">
+              <FaMagnifyingGlassSolid
+                color={colors.charcoal[700]}
+                width={20}
+                height={20}
+              />
+            </View>
+          </Pressable>
+          {isFilterActive ? (
+            <Pressable onPress={() => props.onFilterPress?.()}>
+              <View className="items-center p-2">
+                <Sliders color={colors.charcoal[700]} width={20} height={20} />
+              </View>
+            </Pressable>
+          ) : null}
+        </View>
+      )}
+    />
+  );
+};
+
+// eslint-disable-next-line max-lines-per-function
+export function TodoCatalogue() {
+  const repository = useTodoRepository();
   const searchQuery = useAppSelector(
     (state) => state.todoCatalogueSearch.query,
   );
@@ -90,18 +193,9 @@ export function TodoCatalogue() {
       </Modal>
       <Stack.Screen
         options={{
-          headerRight: () =>
-            isFilterActive ? (
-              <Pressable onPress={() => modal.present()}>
-                <View className="items-center p-4">
-                  <Sliders
-                    color={colors.charcoal[700]}
-                    width={20}
-                    height={20}
-                  />
-                </View>
-              </Pressable>
-            ) : null,
+          header: (props) => (
+            <TodoHeader {...props} onFilterPress={() => modal.present()} />
+          ),
         }}
       />
       <ListPageMemo todos={todos ?? []} />
